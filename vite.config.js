65eec39d27eca422c5ee4f88870404f4dda01479
Eugene Markusheuski <markuszewski.oz@gmail.com>
2023-03-20 00:03:24 +0100

    fix: base address is refixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/vite-deploy-demo',
+    base: '/rss-react/',
     test: {
         globals: true,
         environment: 'jsdom',
