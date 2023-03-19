d6f2f3ad7beac4f70ffd2e7a5c62567829b7a123
Eugene Markusheuski <markuszewski.oz@gmail.com>
2023-03-20 00:09:19 +0100

    fix: build complete

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/rss-react/',
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./src/setupTests.ts'],
-        coverage: {
-            reporter: ['text', 'json', 'html'],
-        },
+  plugins: [react()],
+  base: '',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
     },
+  },
 });
